d1b3d12a74b7aefe91bbbbce2bf925cf6d379995
Rıdvan ÖZKAN <ridvan.ozkan.183@gmail.com>
2025-07-30 13:45:41 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -6,12 +7,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBnqVAhN0dofz1oLEaQh8ZkDkY9eWMX7us",
-  authDomain: "ummahconnect-po02n.firebaseapp.com",
-  projectId: "ummahconnect-po02n",
-  storageBucket: "ummahconnect-po02n.appspot.com",
-  messagingSenderId: "697048800724",
-  appId: "1:697048800724:web:c592f4fffab25e76214a41"
+  "projectId": "ummahconnect-po02n",
+  "appId": "1:697048800724:web:c592f4fffab25e76214a41",
+  "storageBucket": "ummahconnect-po02n.appspot.com",
+  "apiKey": "AIzaSyBnqVAhN0dofz1oLEaQh8ZkDkY9eWMX7us",
+  "authDomain": "ummahconnect-po02n.firebaseapp.com",
+  "messagingSenderId": "697048800724"
 };
 
 
@@ -20,6 +21,6 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getFirestore(app);
 // Explicitly pass the storage bucket URL to ensure correct initialization.
-const storage = getStorage(app, "gs://ummahconnect-po02n.appspot.com");
+const storage = getStorage(app);
 
 export { app, auth, db, storage };
